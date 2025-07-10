68b806db9309d05abcf7f155addf011983584d43
Faizan Shaikh <faizanshaikh.github@gmail.com>
2025-07-10 13:38:24 +0530

    Switched to esbuild to fix Vercel rollup bug

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'process.env': {},
-  },
-  resolve: {
-    alias: {
-      crypto: 'crypto-browserify',
-      stream: 'stream-browserify',
-      buffer: 'buffer',
-    },
-  },
-});
+  build: {
+    target: 'esnext',
+    minify: 'esbuild'
+  }
+})
