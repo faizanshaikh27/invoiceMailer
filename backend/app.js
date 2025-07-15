a4f360f0732cfd252e072477b2e963446c4a8a0a
Faizan Shaikh <faizanshaikh.github@gmail.com>
2025-07-15 13:33:33 +0530

    Cors added

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,11 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://invoice-mailer-b5gi.vercel.app', // or '*'
+  methods: ['GET', 'POST'],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
